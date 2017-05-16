62702d52ab90a28a122714351173703e9d335289
Jeremy McCulloch <jmcculloch2018@francisparker.org>
2017-05-16 13:42:47 -0700

    made insert work

--- submitNewPair.php
+++ submitNewPair.php
@@ -1,47 +1,61 @@
-
-<h1>This is cool</h1>
-
-
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=full-stack", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	print "Connected successfully"; 
 	
 	$first = $_GET['first'];
 	$last = $_GET['last'];
 	$word = $_GET['word'];
 
-	$sql = "SELECT * FROM Teachers WHERE first-name = \"" . $first . "\" AND last-name = \"" . $last . "\";";
-	$teacherRows = $conn->query($sql);
+	$sql = "SELECT * FROM teachers WHERE firstName = \"" . $first . "\" AND lastName = \"" . $last . "\";";
+	$teacherRows = $conn->query($sql)->fetchAll();
 
 	$teacherId;
-	if (teacherRow.length > 0) {
-		$teacherId = teacherRow[0]['id'];
+	if (count($teacherRows) > 0) {
+		$teacherId = $teacherRows[0]['id'];
 	} else {
-		$sql = "INSERT INTO Teachers (first-name, last-name) VALUES (\"" . $first . "\", \"" . $last . "\")";
+		$sql = "INSERT INTO teachers (firstName, lastName) VALUES (\"" . $first . "\", \"" . $last . "\")";
 		$conn->query($sql);
 		$teacherId = $conn->lastInsertId();
 	}
-	foreach ($conn->query($sql) as $row) {
-		print "<tr>";
-        print "<td>" . $row['id'] . "</td>";
-        print "<td>" . $row['name'] . "</td>";
-        print "<td>" . $row['material'] . "</td>";
-		print "</tr>";
-    }
-	print "</table>";
+
+	$sql = "SELECT * FROM words WHERE word = \"" . $word . "\";";
+	$wordRows = $conn->query($sql)->fetchAll();
+
+	$wordId;
+	if (count($wordRows) > 0) {
+		$wordId =$wordRows[0]['id'];
+	} else {
+		$sql = "INSERT INTO words (word) VALUES (\"" . $word . "\")";
+		$conn->query($sql);
+		$wordId = $conn->lastInsertId();
+	}
+
+	$sql = "SELECT * FROM teacherToWord WHERE teacherId = " . $teacherId . " AND wordId = " . $wordId;
+	$teacherToWordRows = $conn->query($sql)->fetchAll();
+
+	if (count($teacherToWordRows) == 0) {
+		$sql = "INSERT INTO teacherToWord (wordId, teacherId, count) VALUES (\"" . $wordId . "\", \"" . $teacherId . "\", 0)";
+		$conn->query($sql);
+	}
+
+	$sql = "SELECT count FROM teacherToWord WHERE teacherId = " . $teacherId . " AND wordId = " . $wordId;
+	$count = $conn->query($sql)->fetchAll()[0]['count'];
+
+	$sql = "UPDATE teacherToWord SET count = " . ($count + 1) . " WHERE teacherId = " . $teacherId . " AND wordId = " . $wordId;
+	$conn->query($sql);
+
 	
-	
+
+	print $first . " " . $last . " is associated with " . $word . " " . ($count + 1) . " times.";
     
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+	
+} catch(PDOException $e) {
+}
+
 ?>
